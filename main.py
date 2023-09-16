561a84d68ca0289aa11aa2bd84eeb5abf59f77e2
promel <jkm.more@gmail.com>
2023-09-16 21:28:45 +0200

    create new directory if it does not exist

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import os
 from PIL import Image
 from resizeimage import resizeimage
 from os import walk
 
-for (dirpath, dirnames, filenames) in walk('./old--images'):
+oldDir ='/'
+newDir = 'new-images/'
+isExist = os.path.exists(newDir)
+if not isExist:
+    os.mkdir(newDir)
+
+for (dirpath, dirnames, filenames) in walk(oldDir):
     for filename in filenames:
         path = dirpath + '/' + filename
         with open(path, 'r+b') as f:
             with Image.open(f) as image:
                 cover = resizeimage.resize_cover(image, [578 , 575])
-                cover.save('new-images/' + filename, image.format)
+                cover.save(newDir + filename, image.format)
